7ac0256edbda9a99cd19171c4d6bafe45df49edb
jaffe9 <jaffar.meeran@gmail.com>
2022-04-10 13:33:29 +0300

    Update server.js
    Trial

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(require('pino-http')({logger: pino}));
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
   req.log.info({message: 'Hello from Node.js Starter Application!'});
-  res.send('Hello from Node.js Starter Application!');
+  res.send('Hello from Node.js J's Starter Application!');
 });
 
 app.get('*', (req, res) => {
